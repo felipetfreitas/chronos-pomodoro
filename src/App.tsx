072c178b6baa2bcade99340b8316c839b7f10fb4
Felipe Rocha <felipet.freitas@hotmail.com>
2025-11-21 00:37:14 -0300

    Css Structurization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Heading } from "./components/Heading";
 
 function App() {
   console.log("App is running");
 
   return (
     <>
-      <Heading attr={123} attr2="String">
-        Chronos Pomodoro 3
-      </Heading>
-      <Heading>Chronos Pomodoro 1</Heading>
-      <Heading>Chronos Pomodoro 2</Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
